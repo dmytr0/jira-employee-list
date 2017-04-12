2634cf9ac9fb3797945efc82bf3592ebb0c02707
Дмитрий Полищук <d.polishchuk@softlist.ua>
2017-04-12 15:04:54 +0300

    Prepared for marketplace

--- src/main/java/ua/softlist/employee/jira/webwork/EmployeesAction.java
+++ src/main/java/ua/softlist/employee/jira/webwork/EmployeesAction.java
@@ -1,5 +1,7 @@
 package ua.softlist.employee.jira.webwork;
 
+import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.user.ApplicationUser;
 import com.atlassian.jira.web.action.JiraWebActionSupport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,10 +9,18 @@ import org.slf4j.LoggerFactory;
 public class EmployeesAction extends JiraWebActionSupport
 {
     private static final Logger log = LoggerFactory.getLogger(EmployeesAction.class);
+    private boolean isHr = false;
 
     @Override
     public String execute() throws Exception {
 
+        ApplicationUser loggedInUser = getLoggedInUser();
+        isHr = ComponentAccessor.getGroupManager().isUserInGroup(loggedInUser, "hr360");
+
         return super.execute();
+    }
+
+    public boolean getIsHr() {
+        return isHr;
     }
 }
